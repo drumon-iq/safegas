b155fe865e3eb0bca6eb3ffc8683c47a6672c10e
drumon-iq <arifdbr@gmail.com>
2025-06-05 17:15:47 -0300

    Adicionando o basico do servidor http
    PROBLEMAS!!!! Delay e o servidor não interagem bem
    1. Se deixarmos o delay, o servidor também fica com delay !1!!1!11
       (obviamente)
    2. Se não deixarmos com delay, teremos que preocupar com certas partes
       do sistema que não lidam muito bem com pouco delay.
    3. Podemos simplesmente deixar o servidor num loop propio ... huh
    4. Testar potenciais problemas de ter um loop dentro de um loop para
       rotinas que dependem de um loop mais não estão dentro do outro loop,
       e um delay dentro do primeiro loop que mais ou menos impede o segundo
           loop??? tem multithreading???

--- safegas/safegas.ino
+++ safegas/safegas.ino
@@ -2,29 +2,39 @@
 #include "sensores.h"
 #include "util.h"
 
+#define SSID "blyeat"
+#define PASS "123456789"
+
 bool doDebug = false;
 
-void setup ()
-{
-  Serial.begin (115200);
+char client_c;
+char old_client_c = '\0';
 
-  setup_sensores ();
-  digitalWrite (relePin, HIGH);
+WiFiServer server(80);
+WiFiClient client;
 
-  set_duration_min (doDebug ? 1 : 5);
+void setup() {
+  Serial.begin(115200);
+
+  setup_sensores();
+
+  set_duration_min(doDebug ? 1 : 5);
   timer = 0;
   old_millis = 0;
+
+  WiFi.softAP(SSID, PASS);
+  server.begin();
+  WiFiClient client = server.available();
 }
 
-void loop ()
-{
+void loop() {
   unsigned long int curr_millis = millis();
 
-  digitalWrite (LED, LOW);
-  tone (buzzer, 0);
+  digitalWrite(LED, LOW);
+  tone(buzzer, 0);
 
-  Serial.print ("Timer: ");
-  Serial.println (timer);
+  Serial.print("Timer: ");
+  Serial.println(timer);
 
   if (doDebug || (!movementDetected() && flameDetected()))
     timer += curr_millis - old_millis;
@@ -35,10 +45,32 @@ void loop ()
     toggleValvula(false);
   else if (timer >= duration) {
     doAlert = !doAlert; //flip pra depois flop
-    digitalWrite (LED, doAlert ? HIGH : LOW);
-    tone (buzzer, doAlert ? 4000 : 0);
+    digitalWrite(LED, doAlert ? HIGH : LOW);
+    tone(buzzer, doAlert ? 4000 : 0);
   }
 
-  delay (100);
+  if (client) {
+    if (client.available() && client.connected()) {
+
+      client_c = client.read();
+      Serial.write(old_client_c);
+
+      if (old_client_c == '\r' && client_c == '\n') {
+        client.println("HTTP/1.1 200 OK");
+        client.println("Content-type:text/html");
+        client.println("Connection: close");
+        client.println();
+        client.println("HELLO WORLD");
+        client.println();
+
+        client.stop();
+      }
+      old_client_c = client_c;
+    }
+  } else {
+    client = server.available();
+    old_client_c = 0;
+  }
+
   old_millis = curr_millis;
 }
